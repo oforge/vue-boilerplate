27662e851172f83c8e1ce4ec52b8c36917b39113
Der-Alex <alexander.thimm@googlemail.com>
2020-09-06 18:31:07 +0200

    fixed typo

--- src/components/global/main.ts
+++ src/components/global/main.ts
@@ -2,13 +2,13 @@
   Here we define all globally usable components. Something like form elements or maybe icons.
   This object can be imported into the src/main.ts. There you can iterate over the objects and load them via vue.component(name, value)
  */
-import SampleComponent from '@/components/global/OtherSampleComponent.vue';
+import OtherSampleComponent from '@/components/global/OtherSampleComponent.vue';
 import { VueConstructor } from 'vue';
 
 export default {
   'sample-component': () =>
     import(/* webpackChunkName: "globalComponents" */ './SampleComponent.vue'),
-  'other-sample-component': SampleComponent as VueConstructor,
+  'other-sample-component': OtherSampleComponent as VueConstructor,
   'app-icon': () =>
     import(/* webpackChunkName: "globalComponents" */ './AppIcon.vue'),
   'app-button': () =>
